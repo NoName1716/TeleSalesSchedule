65c971d176c0a86870e2add54554e3006deeca98
sktomov <sktomov@gmail.com>
2017-04-11 20:54:16 +0300

    Adding Models -> Schedule.cs, Computer.cs, Employee.cs

--- TelesalesSchedule/Models/TelesalesScheduleDbContext.cs
+++ TelesalesSchedule/Models/TelesalesScheduleDbContext.cs
@@ -1,8 +1,5 @@
-﻿using System.Data.Entity;
-using System.Security.Claims;
-using System.Threading.Tasks;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
 
 namespace TelesalesSchedule.Models
 {
@@ -12,6 +9,12 @@ namespace TelesalesSchedule.Models
             : base("DefaultConnection", throwIfV1Schema: false)
         {
         }
+
+        public virtual DbSet<Employee> Employees { get; set; }
+
+        public virtual DbSet<Schedule> Schedules { get; set; }
+
+        public virtual DbSet<Computer> Computers { get; set; }
 
         public static TelesalesScheduleDbContext Create()
         {

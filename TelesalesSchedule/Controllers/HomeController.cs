aa33197ca48bd4fe9a1ff78e7615d93958a79b41
sktomov <sktomov@gmail.com>
2017-04-20 08:33:44 +0300

    Index Page - PreviousWeek & NextWeek

--- TelesalesSchedule/Controllers/HomeController.cs
+++ TelesalesSchedule/Controllers/HomeController.cs
@@ -22,22 +22,57 @@ namespace TelesalesSchedule.Controllers
                 //for test only
                 monday = new DateTime(2017, 04, 24);
                 sunday = new DateTime(2017, 04, 30);
+                ViewBag.StartDate = monday.Date.ToShortDateString();
+                ViewBag.EndDate = sunday.Date.ToShortDateString();
                 var computers = db.Schedules.Where(s => s.StartDate == monday.Date && s.EndDate == sunday.Date).Select(p => p.Computer).ToList();
 
                 var comps = db.Schedules.Where(s => s.StartDate == monday.Date && s.EndDate == sunday.Date).Where(p => p.Computer != null && p.Computer.IsWorking == true).ToList();
 
-                //var comps = db.Schedules.Include("Computers").Where(s => s.StartDate == monday.Date && s.EndDate == sunday.Date).Select(s => new
-                //{
-                //    name = s.Computer.Name,
-                //    mondayShiftOne = s.MondayShiftOneEnd ?? 0 - s.MondayShiftOneStart ?? 0,
-                //    mondayShiftTwo = s.MondayShiftTwoEnd ?? 0 - s.MondayShiftTwoStart ?? 0,
-                //    mondayShiftThree = s.MondayShiftThreeEnd ?? 0 - s.MondayShiftThreeStart ?? 0,
-                //    thuesdayShiftOne = s.ThuesdayShiftOneEnd ?? 0 - s.ThuesdayShiftOneStart ?? 0,
-                //    thuesdayShiftTwo = s.ThuesdayShiftTwoEnd ?? 0 - s.ThuesdayShiftTwoStart ?? 0,
-                //    thuesdayShiftThree = s.ThuesdayShiftThreeEnd ?? 0 - s.ThuesdayShiftThreeStart ?? 0,
+                return View(comps);
+            }
+        }
+        public ActionResult NextWeek()
+        {
+            using (var db = new TelesalesScheduleDbContext())
+            {
 
-                //}).ToList();
+                DateTime monday = DateTime.Now;
 
+                while (monday.DayOfWeek != DayOfWeek.Monday)
+                {
+                    monday = monday.AddDays(1);
+                }
+                var sunday = monday.AddDays(6);
+                
+                ViewBag.StartDate = monday.Date.ToShortDateString();
+                ViewBag.EndDate = sunday.Date.ToShortDateString();
+                var computers = db.Schedules.Where(s => s.StartDate == monday.Date && s.EndDate == sunday.Date).Select(p => p.Computer).ToList();
+
+                var comps = db.Schedules.Where(s => s.StartDate == monday.Date && s.EndDate == sunday.Date).Where(p => p.Computer != null && p.Computer.IsWorking == true).ToList();
+
+                return View(comps);
+            }
+        }
+        public ActionResult PreviousWeek()
+        {
+            using (var db = new TelesalesScheduleDbContext())
+            {
+
+                DateTime monday = DateTime.Now;
+
+                while (monday.DayOfWeek != DayOfWeek.Monday)
+                {
+                    monday = monday.AddDays(-1);
+                }
+                
+                monday = monday.AddDays(-7);
+                var sunday = monday.AddDays(6);
+
+                ViewBag.StartDate = monday.Date.ToShortDateString();
+                ViewBag.EndDate = sunday.Date.ToShortDateString();
+                var computers = db.Schedules.Where(s => s.StartDate == monday.Date && s.EndDate == sunday.Date).Select(p => p.Computer).ToList();
+
+                var comps = db.Schedules.Where(s => s.StartDate == monday.Date && s.EndDate == sunday.Date).Where(p => p.Computer != null && p.Computer.IsWorking == true).ToList();
 
                 return View(comps);
             }
